a6d9b0d3612f8c2ed37a6fe309f238481f9ecdba
srodney <steve.rodney@gmail.com>
2017-03-17 10:42:03 -0400

    more smoothing!

--- salt2ir.py
+++ salt2ir.py
@@ -923,6 +923,8 @@ def extend_template1_ir(modeldict = None, cmin=-0.1, cmax=0.3,
                         salt2dir = 'salt2-4',
                         salt2irdir = 'salt2ir',
                         wavejoinstart = 8500, wavemax = 25000,
+                        wavesmoothwindow=100,
+                        timesmoothwindow=5,
                         showphase0plots=False):
     """ extend the M1 component of the SALT2 model into the IR.  (this is the
     component that is multiplied by the SALT2 parameter x1, and therefore
@@ -934,9 +936,13 @@ def extend_template1_ir(modeldict = None, cmin=-0.1, cmax=0.3,
     :param salt2irdir:
     :param wavejoinstart:
     :param wavemax:
+    :param wavesmoothwindow: smoothing window size in Angstrom
     :param showphase0plots:
     :return:
     """
+    medsmooth = lambda f, N : np.array([np.median(
+        f[max(0,i-N):min(len(f),max(0,i-N)+2*N)]) for i in range(len(f))])
+
     if modeldict == None:
         modeldict = load_sncosmo_models()
     salt2dir = os.path.join(modeldir, salt2dir)
@@ -988,6 +994,7 @@ def extend_template1_ir(modeldict = None, cmin=-0.1, cmax=0.3,
     newM1weight = whtslope * wavelist_join + whtintercept
     oldM1weight = 1 - newM1weight
 
+    newM1grid = []
     for iphase1 in range(len(temp1.phase)): # iphaselist:
         thisphase = temp1.phase[iphase1]
 
@@ -1035,9 +1042,13 @@ def extend_template1_ir(modeldict = None, cmin=-0.1, cmax=0.3,
             print("only %i templates for phase = %.1f." % (
                 len(M1extlist), thisphase))
             print("Using M1(lambda)=0 for all IR wavelengths")
-            newM1median = np.zeros(len(wavelist_all))
+            newM1median = np.zeros(len(wavelist_ir))
         else:
             newM1median = np.median(M1extlist, axis=0)
+
+        if wavesmoothwindow:
+            Nsmooth = int(wavesmoothwindow / wavestep)
+            newM1median = medsmooth(newM1median, Nsmooth)
 
         # apply the predefined weight functions to combine the new M1
         # curve with the old M1 curve in the "join window"
@@ -1049,6 +1060,10 @@ def extend_template1_ir(modeldict = None, cmin=-0.1, cmax=0.3,
         newM1values = np.append(
             np.append(temp1.value[iphase1][:ijoinold[0]], newM1joinvalues),
             newM1median[ijoinnew[-1]+1:])
+        newM1grid.append(newM1values)
+
+        if len(newM1values) != len(wavelist_all):
+            import pdb; pdb.set_trace()
 
         if showphase0plots and thisphase==0:
             fig1 = pl.figure(1)
@@ -1060,6 +1075,17 @@ def extend_template1_ir(modeldict = None, cmin=-0.1, cmax=0.3,
             ax1.plot(wavelist_all, newM1values, lw=1, color='k')
             ax1.plot(temp1.wave[0], M1func(temp1.wave[0]), color='r', lw=2.5)
 
+    newM1grid = np.array(newM1grid)
+
+    if timesmoothwindow:
+        for iwave in range(len(wavelist_all)):
+            newM1grid[:,iwave] = savitzky_golay(
+                newM1grid[:,iwave], timesmoothwindow)
+
+    for iphase1 in range(len(temp1.phase)): # iphaselist:
+        thisphase = temp1.phase[iphase1]
+        newM1values = newM1grid[iphase1]
+
         # append to the list of output data lines
         for j in range(len(wavelist_all)) :
             outlines.append( "%6.2f    %12i  %12.7e\n"%(
@@ -1069,6 +1095,7 @@ def extend_template1_ir(modeldict = None, cmin=-0.1, cmax=0.3,
     fout = open( temp1fileOUT, 'w' )
     fout.writelines( outlines )
     fout.close()
+    return(newM1grid)
 
 
 def plot_extended_template1(phaselist=[-15,-5,0,5,25]):
@@ -1396,3 +1423,102 @@ def mk_salt2ir_template0_plots():
     ax.set_xlabel('wavelength (Angstrom)')
     pl.draw()
 
+
+def plot_salt2_component_timeseries(
+        component=0, wavemin=11000, wavemax=14000,
+        modeldir='/Users/rodney/Dropbox/WFIRST/SALT2IR',
+        salt2dir = 'salt2-4', salt2irdir = 'salt2ir',
+        **kwargs):
+    salt2dir = os.path.join(modeldir, salt2dir)
+    salt2irdir = os.path.join(modeldir, salt2irdir)
+
+    temp0fileIR = os.path.join( salt2irdir, 'salt2_template_0.dat' )
+    temp1fileIR = os.path.join( salt2irdir, 'salt2_template_1.dat' )
+
+    if component == 1 :
+        valuegrid = TimeSeriesGrid(temp1fileIR)
+    else:
+        valuegrid = TimeSeriesGrid(temp0fileIR)
+
+    wavelist = np.linspace(wavemin, wavemax, 5)
+    for wave in wavelist:
+        phaselist = range(-5,10)
+        iwave = np.argmin(np.abs(valuegrid.wave[0]-wave))
+        iphaselist = np.array([np.where(valuegrid.phase == phase)[0][0]
+                               for phase in phaselist])
+        valuelist = np.array([valuegrid.value[ip][iwave] for ip in iphaselist])
+        pl.plot(phaselist, valuelist, label='%i Ang' % wave, **kwargs)
+    ax = pl.gca()
+    ax.set_xlabel('time (days)')
+    ax.set_ylabel('SALT2 model component M%i value' % component)
+    ax.legend(loc='best', ncol=2)
+
+
+def savitzky_golay(y, window_size=5, order=3, deriv=0):
+    """Smooth (and optionally differentiate) data with a Savitzky-Golay filter.
+    The Savitzky-Golay filter removes high frequency noise from data.
+    It has the advantage of preserving the original shape and
+    features of the signal better than other types of filtering
+    approaches, such as moving averages techhniques.
+    Parameters
+    ----------
+    y : array_like, shape (N,)
+        the values of the time history of the signal.
+    window_size : int
+        the length of the window. Must be an odd integer number.
+    order : int
+        the order of the polynomial used in the filtering.
+        Must be less then `window_size` - 1.
+    deriv: int
+        the order of the derivative to compute (default = 0 means only smoothing)
+    Returns
+    -------
+    ys : ndarray, shape (N)
+        the smoothed signal (or it's n-th derivative).
+    Notes
+    -----
+    The Savitzky-Golay is a type of low-pass filter, particularly
+    suited for smoothing noisy data. The main idea behind this
+    approach is to make for each point a least-square fit with a
+    polynomial of high order over a odd-sized window centered at
+    the point.
+    Examples
+    --------
+    t = np.linspace(-4, 4, 500)
+    y = np.exp( -t**2 ) + np.random.normal(0, 0.05, t.shape)
+    ysg = savitzky_golay(y, window_size=31, order=4)
+    import matplotlib.pyplot as plt
+    plt.plot(t, y, label='Noisy signal')
+    plt.plot(t, np.exp(-t**2), 'k', lw=1.5, label='Original signal')
+    plt.plot(t, ysg, 'r', label='Filtered signal')
+    plt.legend()
+    plt.show()
+    References
+    ----------
+    .. [1] A. Savitzky, M. J. E. Golay, Smoothing and Differentiation of
+       Data by Simplified Least Squares Procedures. Analytical
+       Chemistry, 1964, 36 (8), pp 1627-1639.
+    .. [2] Numerical Recipes 3rd Edition: The Art of Scientific Computing
+       W.H. Press, S.A. Teukolsky, W.T. Vetterling, B.P. Flannery
+       Cambridge University Press ISBN-13: 9780521880688
+    """
+    try:
+        window_size = np.abs(np.int(window_size))
+        order = np.abs(np.int(order))
+    except ValueError, msg:
+        raise ValueError("window_size and order have to be of type int")
+    if window_size % 2 != 1 or window_size < 1:
+        raise TypeError("window_size size must be a positive odd number")
+    if window_size < order + 2:
+        raise TypeError("window_size is too small for the polynomials order")
+    order_range = range(order+1)
+    half_window = (window_size -1) // 2
+    # precompute coefficients
+    b = np.mat([[k**i for i in order_range] for k in range(-half_window, half_window+1)])
+    m = np.linalg.pinv(b).A[deriv]
+    # pad the signal at the extremes with
+    # values taken from the signal itself
+    firstvals = y[0] - np.abs( y[1:half_window+1][::-1] - y[0] )
+    lastvals = y[-1] + np.abs(y[-half_window-1:-1][::-1] - y[-1])
+    y = np.concatenate((firstvals, y, lastvals))
+    return np.convolve( m, y, mode='valid')
